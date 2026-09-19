f9ce6bf264d014237c7bfa0c96492cd53d5ce1ec
cerudn <albertoceruelo@gmail.com>
2026-09-19 12:16:26 +0200

    Integrate first RGB565 sprite

--- include/render/SpriteRenderer.h
+++ include/render/SpriteRenderer.h
@@ -1,21 +1,20 @@
 #pragma once
-#include "hal/Display.h"
-#include <cstdint>
 
-namespace dtec::render {
+#include "../hal/Display.h"
+
+namespace render {
 
 class SpriteRenderer {
 public:
-    explicit SpriteRenderer(dtec::hal::IDisplay& display) : display_(display) {}
+    explicit SpriteRenderer(hal::Display& display);
 
-    // Blit single frame from PROGMEM array
-    void blitSprite(int x, int y, int w, int h, const uint16_t* frameData, bool transparent);
-
-    // Blit specific frame from frame array
-    void blitSpriteFrame(int x, int y, int w, int h, const uint16_t* const* frames, uint8_t frameIndex, bool transparent);
+    /**
+     * Draw an RGB565 bitmap at the specified screen position.
+     */
+    void drawBitmap(int x, int y, const hal::Bitmap& bitmap);
 
 private:
-    dtec::hal::IDisplay& display_;
+    hal::Display& display_;
 };
 
-} // namespace dtec::render
+} // namespace render
