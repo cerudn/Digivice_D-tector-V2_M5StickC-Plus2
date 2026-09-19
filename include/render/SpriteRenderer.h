3fb5dc0294aad798d0c368064b6e6f992618381a
cerudn <albertoceruelo@gmail.com>
2026-09-19 12:31:46 +0200

    Integrate first RGB565 sprite

--- include/render/SpriteRenderer.h
+++ include/render/SpriteRenderer.h
@@ -7,10 +7,6 @@ namespace render {
 class SpriteRenderer {
 public:
     explicit SpriteRenderer(hal::Display& display);
-
-    /**
-     * Draw an RGB565 bitmap at the specified screen position.
-     */
     void drawBitmap(int x, int y, const hal::Bitmap& bitmap);
 
 private:
